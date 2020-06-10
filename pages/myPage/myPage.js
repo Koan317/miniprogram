b51f664b14dd32c650b1ec0b9208148113cdf82d
Koan <koan317@qq.com>
2020-06-10 14:25:40 +0800

    2020/6/10_signin
    signin_alpha

--- pages/myPage/myPage.js
+++ pages/myPage/myPage.js
@@ -1,18 +1,54 @@
 // miniprogram/pages/myPage/myPage.js
+const app=getApp()
 Page({
-
 	/**
 	 * 页面的初始数据
 	 */
 	data: {
-
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+		logged: false,
+		resId:'',
+		isSignIn:false
 	},
 
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
 	onLoad: function (options) {
+		if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
 
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+
+		function v(e) {
+			if (!this.data.logged && e.detail.userInfo) {
+				this.setData({
+					logged: true,
+					avatarUrl: e.detail.userInfo.avatarUrl,
+					userInfo: e.detail.userInfo
+				})
+			}
+		}
 	},
 
 	/**
@@ -29,6 +65,55 @@ Page({
 
 	},
 
+	onSignIn(){
+		var date=new Date()
+		//var isSignIn
+		const db = wx.cloud.database()
+
+		db.collection('signin').where({
+			signInDate:date.getFullYear()+'/'+date.getMonth()+'/'+date.getDate(),
+			_openid:app.openid
+		}).get({
+			success:signHistory=>{
+				if(signHistory.data.length){
+					console.log('今日已签到')
+					wx.showToast({
+						title: '今日已签到'
+					})
+					this.setData({
+						isSignIn:true
+					})
+				}
+			}
+		})
+		console.log(this.data.isSignIn)
+		if(!this.data.isSignIn){
+			db.collection('signin').add({
+				data: {
+					openId:app.openid,
+					signInDate:date.getFullYear()+'/'+date.getMonth()+'/'+date.getDate(),
+					signInTime:date.getHours()+':'+date.getMinutes()+':'+date.getSeconds()
+				},
+				success: signInres => {
+					// 在返回结果中会包含新创建的记录的 _id
+					this.setData({
+						resId:signInres._id,
+					})
+					wx.showToast({
+						title: '签到成功'
+					})
+					console.log('签到成功，记录 _id: ', signInres._id)
+				},
+				fail: err => {
+					wx.showToast({
+						icon: 'none',
+						title: '签到失败'
+					})
+					console.error('[数据库] [新增记录] 失败：', err)
+				}
+			})
+		}
+	},
 	/**
 	 * 生命周期函数--监听页面隐藏
 	 */
